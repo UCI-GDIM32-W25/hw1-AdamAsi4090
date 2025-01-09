92972f160450adeb14ed37f68793f1c10f4713d2
AdamAsi4090 <ajasi@uci.edu>
2025-01-09 14:00:05 -0800

    Still need to figure out how to get the plants to work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -52,7 +52,7 @@ public class Player : MonoBehaviour
     {
         Debug.Log("The plant has been planted!");
         //Instantiates the plant object into the world
-        GameObject plant = Instantiate(_plantPrefab);
+        GameObject plant = Instantiate(_plantPrefab, _playerTransform.position);
         //Decrements the amount of seeds left
         _numSeedsLeft--;
         //Increments the amount of seeds planted
