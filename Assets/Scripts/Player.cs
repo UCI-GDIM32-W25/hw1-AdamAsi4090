7866b51f90f938c871a265eb698047e2d61d5378
AdamAsi4090 <ajasi@uci.edu>
2025-01-09 12:38:57 -0800

    Movement done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,26 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        //Moves the player up if W or up-arrow is pressed
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+        //Moves the player down if S or down-arrow is pressed
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        //Moves the player left if S or left-arrow is pressed
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        //Moves the player right if D or right-arrow is pressed.
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
     }
 
     public void PlantSeed ()
