5ad74ed5ae672f76494c90037750c11f46bddd5b
AdamAsi4090 <ajasi@uci.edu>
2025-01-09 13:03:04 -0800

    UI works

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,10 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        //Sets the number of seeds left to the number of seeds
+        _numSeedsLeft = _numSeeds;
+        //Put the number of seeds and the current number of seeds planted into UI
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, 0);
     }
 
     private void Update()
@@ -21,27 +24,40 @@ public class Player : MonoBehaviour
         //Moves the player up if W or up-arrow is pressed
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up * _speed * Time.deltaTime);
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
         }
         //Moves the player down if S or down-arrow is pressed
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
         }
         //Moves the player left if S or left-arrow is pressed
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
         }
         //Moves the player right if D or right-arrow is pressed.
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        //Plants a seed if space is pressed, and the number of seeds doesn't equal 0
+        if (Input.GetKey(KeyCode.Space) && _numSeedsLeft != 0)
+        {
+            PlantSeed();
         }
     }
 
     public void PlantSeed ()
     {
-        
+        Debug.Log("The plant has been planted!");
+        //Instantiates the plant object into the world
+        GameObject plant = Instantiate(_plantPrefab);
+        //Decrements the amount of seeds left
+        _numSeedsLeft--;
+        //Increments the amount of seeds planted
+        _numSeedsPlanted++;
+        //Updates the UI
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
